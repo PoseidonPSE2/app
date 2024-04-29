59ee7adb603389592ab6b13f41c99cb3ad600337
HtoO <HtoO.MtoL@uni.com>
2024-04-29 21:58:46 +0200

    Dev: adding a confirmation when sending the request

--- lib/ui/station.dart
+++ lib/ui/station.dart
@@ -7,40 +7,7 @@ import 'package:nfc_manager/nfc_manager.dart';
 
 String _responseText = '';
 
-void _makeAPICall_post(String stationsName, double waterAmount) async {
-  print("go");
 
-  Map<String, dynamic> data = {
-    'water': waterAmount, // Example value, replace with your actual data
-    'user_id': 1, // Example value, replace with your actual data
-    'water_type': stationsName // Example value, replace with your actual data
-  };
-
-  print("create json body");
-  // Encode the data to JSON
-  var body = json.encode(data);
-
-  print("send request");
-  // Make POST request
-  final response = await http.post(
-    Uri.parse('https://2sc10r.buildship.run/create_entry'),
-    headers: {
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: body,
-  );
-  print(body);
-
-  print("wait for answer");
-  if (response.statusCode == 200) {
-    // Handle successful API call
-    _responseText = response.body;
-  } else {
-    // Handle error
-    _responseText = 'Failed to make API call';
-  }
-  print(_responseText);
-}
 
 class RefillScreen extends StatefulWidget {
   @override
@@ -51,6 +18,8 @@ class _RefillScreenState extends State<RefillScreen> {
   String _text = '';
   final ValueNotifier<String> _nfcResult = ValueNotifier(''); // Updated type
   double _waterAmount = 0.0;
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
 
   Future<void> _showWaterInputDialog() async {
     return showDialog<void>(
@@ -76,7 +45,8 @@ class _RefillScreenState extends State<RefillScreen> {
               child: Text('Bestätigen'),
               onPressed: () {
                 Navigator.of(context).pop();
-                _makeAPICall_post(_text, _waterAmount);
+                String water = _waterAmount.toString();
+                _makeAPICall_post(_text, water);
               },
             ),
           ],
@@ -106,6 +76,7 @@ class _RefillScreenState extends State<RefillScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: navigatorKey,
       backgroundColor: Color.fromRGBO(8, 52, 76, 1),
       body: Center(
         child: Column(
@@ -159,6 +130,7 @@ class _RefillScreenState extends State<RefillScreen> {
                 var payloadBytes = entry['payload'];
                 String text = utf8.decode(payloadBytes);
                 text = text.substring(3);
+                _text = text;
               }
             }
           }
@@ -170,5 +142,45 @@ class _RefillScreenState extends State<RefillScreen> {
     } catch (e) {
       print('Error reading NFC: $e');
     }
+  }
+  void _makeAPICall_post(String stationsName, String waterAmount) async {
+    print("go");
+
+    Map<String, dynamic> data = {
+      'water': waterAmount, // Example value, replace with your actual data
+      'user_id': 1, // Example value, replace with your actual data
+      'water_type': stationsName // Example value, replace with your actual data
+    };
+
+    print("create json body");
+    // Encode the data to JSON
+    var body = json.encode(data);
+
+    print("send request");
+    // Make POST request
+    final response = await http.post(
+      Uri.parse('https://2sc10r.buildship.run/create_entry'),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: body,
+    );
+    print(body);
+
+    print("wait for answer");
+    if (response.statusCode == 200) {
+      // Handle successful API call
+      _responseText = response.body;
+      ScaffoldMessenger.of(navigatorKey.currentContext!).showSnackBar(
+        SnackBar(
+          content: Text('Erfolgreich! Daten an API gesendet.'),
+          backgroundColor: Colors.green,
+        ),
+      );
+    } else {
+      // Handle error
+      _responseText = 'Failed to make API call';
+    }
+    print(_responseText);
   }
 }
